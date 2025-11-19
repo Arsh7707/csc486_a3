3050b89fecb9330813fcfb990fbed79c4221e5dc
Brandon Haworth <bhaworth@uvic.ca>
2025-11-19 00:37:15 -0800

    Initial commit

---
+++ Assets/Scripts/Swarm.cs
@@ -0,0 +1,128 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Swarm : MonoBehaviour
+{
+    public struct BBoid
+    {
+        public Vector3 position;
+        public Vector3 forward;
+        public Vector3 velocity;
+        public Vector3 alignment;
+        public Vector3 cohesion;
+        public Vector3 separation;
+        public Vector3 obstacle;
+        public Vector3 currentTotalForce;
+    }
+
+    public Transform boidPrefab;
+
+    public int numberOfBoids = 200;
+
+    public float boidForceScale = 20f;
+
+    public float maxSpeed = 5.0f;
+
+    public float rotationSpeed = 40.0f;
+
+    public float obstacleCheckRadius = 1.0f;
+
+    public float separationWeight = 1.1f;
+    
+    public float alignmentWeight = 0.5f;
+
+    public float cohesionWeight = 1f;
+
+    public float goalWeight = 1f;
+
+    public float obstacleWeight = 0.9f;
+
+    public float neighbourDistance = 2.0f;
+
+    public float initializationRadius = 1.0f;
+
+    public float initializationForwardRandomRange = 50f;
+
+    private BBoid[] boids;
+
+    private Transform[] boidObjects;
+
+    private float sqrNeighbourDistance;
+
+    private Vector3 boidZeroGoal;
+    private NavMeshPath boidZeroPath;
+    private int currentCorner;
+    private bool boidZeroNavigatingTowardGoal = false;
+
+
+    /// <summary>
+    /// Start, this function is called before the first frame
+    /// </summary>
+    private void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// Initialize the array of boids
+    /// </summary>
+    private void InitBoids()
+    {
+       
+    }
+
+
+    /// <summary>
+    /// Reset the particle forces
+    /// </summary>
+    public void ResetBoidForces()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// Sim Loop
+    /// </summary>
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        /* Render information for boidzero, useful for debugging forces and path planning
+        int boidCount = boids.Length;
+        for (int i = 1; i < boidCount; i++)
+        {
+            Vector3 boidNeighbourVec = boids[i].position - boids[0].position;
+            if (boidNeighbourVec.sqrMagnitude < sqrNeighbourDistance &&
+                    Vector3.Dot(boidNeighbourVec, boids[0].forward) > 0f)
+            { 
+                Debug.DrawLine(boids[0].position, boids[i].position, Color.blue);
+            }
+        }
+        
+        Debug.DrawLine(boids[0].position, boids[0].position + boids[0].alignment, Color.green);
+        Debug.DrawLine(boids[0].position, boids[0].position + boids[0].separation, Color.magenta);
+        Debug.DrawLine(boids[0].position, boids[0].position + boids[0].cohesion, Color.yellow);
+        Debug.DrawLine(boids[0].position, boids[0].position + boids[0].obstacle, Color.red);
+
+        if (boidZeroPath != null)
+        {
+            int cornersLength = boidZeroPath.corners.Length;
+            for (int i = 0; i < cornersLength - 1; i++)
+                Debug.DrawLine(boidZeroPath.corners[i], boidZeroPath.corners[i + 1], Color.black);
+        }
+        */
+    }
+
+
+    public void SetGoal(Vector3 goal)
+    {
+
+    }
+}
+
